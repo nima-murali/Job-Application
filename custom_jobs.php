fd6a336d23ad60f341b1ecb7bd99e60c7e391110
Nima <nimamuralis@gmail.com>
2021-10-13 16:41:03 +0530

    added textarea,text,select,and radio button fields in settings page and created short code for adding jobs to widgets

--- custom_jobs.php
+++ custom_jobs.php
@@ -3,6 +3,7 @@
 class Wpcustomjobs{
 	public function __construct(){
 		add_action('init',array($this, 'wp_jobs_post_types'));
+		add_shortcode( 'job-post-list', array($this, 'wp_create_shortcode_for_jobs' )); 
 	}
 
 	public function wp_jobs_post_types(){
@@ -20,6 +21,37 @@ class Wpcustomjobs{
 			'has_archive' => true,
 		));
 	}
+
+
+	public function wp_create_shortcode_for_jobs(){
+		$args = array(
+			'post_type'      => 'jobs',
+            'posts_per_page' => '10',
+            'publish_status' => 'published',
+        );
+  
+    	$query = new WP_Query($args);
+  
+    	if($query->have_posts()) :
+  
+        	while($query->have_posts()) :
+  
+            	$query->the_post() ;
+                      
+        		$result .= '<div class="job-item">';
+        		$result .= '<div class="job-name">' . get_the_title() . '</div>';
+        		$result .= '<div class="job-desc">' . get_the_content() . '</div>'; 
+        		$result .= '</div>';
+  
+        	endwhile;
+  
+        	wp_reset_postdata();
+    	endif;    
+  
+    	return $result;            
+}
+  
+
 }
 
 $objjob	=	new Wpcustomjobs();
--- settings.php
+++ settings.php
@@ -2,8 +2,7 @@
 class Wpsubmenu{
 	public function __construct(){
 		add_action('admin_menu',array($this, 'wp_register_submenu_settings'));
-		add_action( 'admin_init', array( $this, 'setup_sections' ) );
-		add_action( 'admin_init', array( $this, 'setup_fields' ) );
+		add_action( 'admin_init', array( $this, 'setup_sections_fields' ) );
 	}
 
 	public function wp_register_submenu_settings() {
@@ -13,26 +12,153 @@ class Wpsubmenu{
 	public function wp_submenu_settings_callback() {?>
     	<div class="wrap">
         	<h2>Settings Page</h2>
-        	<form method="post" action="settings.php">
+        	<form method="post" action="options.php">
             	<?php
                 	settings_fields( 'job-settings' );
                 	do_settings_sections( 'job-settings' );
-                	submit_button();
+                	submit_button('Save settings');
             	?>
         	</form>
     	</div> <?php
 	}
-	public function setup_sections() {
-		add_settings_section( 'general_setting_section', 'General Settings', array( $this, 'general_setting_callback' ), 'job-settings' );
+
+	public function setup_sections_fields(){
+
+		// Setup settings section
+		add_settings_section(
+        	'general_settings_section',
+        	'General Settings',
+        	'',
+        	'job-settings'
+    	);
+
+		// Registe input field
+		register_setting(
+        	'job-settings',
+        	'general_settings_input_field',
+        	array(
+            	'type' => 'string',
+            	'sanitize_callback' => 'sanitize_text_field',
+            	'default' => ''
+        	)
+        );
+
+		// Add text fields
+        add_settings_field(
+        	'general_settings_input_field',
+        	'Input Field',
+        	array( $this, 'general_settings_input_field_callback'),
+        	'job-settings',
+        	'general_settings_section'
+    	);
+
+        // Registe textarea field
+    	register_setting(
+        	'job-settings',
+        	'general_settings_textarea_field',
+        	array(
+            	'type' => 'string',
+            	'sanitize_callback' => 'sanitize_textarea_field',
+            	'default' => ''
+        	)
+    	);
+
+    	// Add textarea fields
+     	add_settings_field(
+        	'general_settings_textarea_field',
+        	'About the Company',
+        	array( $this, 'general_settings_textarea_field_callback'),
+        	'job-settings',
+        	'general_settings_section'
+    	);
+
+     	// Registe select field
+    	register_setting(
+        	'job-settings',
+        	'general_settings_select_field',
+        	array(
+            	'type' => 'string',
+            	'sanitize_callback' => 'sanitize_text_field',
+            	'default' => ''
+        	)
+    	);
+
+    	// Add select fields
+     	add_settings_field(
+        	'general_settings_select_field',
+        	'select',
+        	array( $this, 'general_settings_select_field_callback'),
+        	'job-settings',
+        	'general_settings_section'
+    	);
+
+    	// register radio fields
+     	register_setting(
+        	'job-settings',
+        	'general_settings_radio_field',
+        	array(
+            	'type' => 'string',
+            	'sanitize_callback' => 'sanitize_text_field',
+            	'default' => ''
+        	)
+    	);
+
+    	// Add radio fields
+    	add_settings_field(
+        	'general_settings_radio_field',
+        	'Radio Field',
+        	array( $this, 'myplugin_settings_radio_field_callback'),
+        	'job-settings',
+        	'general_settings_section'
+    	);
+
 	}
-	public function general_setting_callback( $arguments ) {
-		echo '';
+	public function general_settings_input_field_callback(){
+		$get_input_field = get_option('general_settings_input_field');
+		?>
+    	<input type="text" name="general_settings_input_field" class="regular-text" value="<?php echo isset($get_input_field) ? esc_attr( $get_input_field ) : ''; ?>" />
+    	<?php 
 	}
-	public function setup_fields() {
-		add_settings_field( 'email_field', 'Email', array( $this, 'email_field_callback' ), 'job-settings', 'general_setting_section' );
+	
+
+	public function general_settings_textarea_field_callback() {
+    	$get_textarea_field = get_option('general_settings_textarea_field');
+    	$get_checkbox_field = get_option('display');
+    	?>
+    	<textarea name="general_settings_textarea_field" class="large-text" rows="5"><?php echo isset($get_textarea_field) ? esc_textarea( $get_textarea_field ) : ''; ?></textarea>
+    	<?php
+    	if($get_checkbox_field == 'True'){ ?>
+			<input type="checkbox" id="display" name="display" value="True" checked>Display Content</input>
+		<?php
+		}
+		else{ ?>
+			<input type="checkbox" id="display" name="display" value="True">Display Content</input>
+		<?php
+		}
 	}
-	public function email_field_callback( $arguments ) {
-		echo '<input name="email_field" id="email_field" type="text" value="' . get_option( 'email_field' ) . '" />';
+
+
+	public function general_settings_select_field_callback(){
+		$get_select_field = get_option('general_settings_select_field');
+		?>
+    	<select name="general_settings_select_field" class="regular-text">
+        	<option value="">--select--</option>
+        	<option value="option1" <?php selected( 'option1', $get_select_field ); ?> >Option 1</option>
+        	<option value="option2" <?php selected( 'option2', $get_select_field ); ?>>Option 2</option>
+    	</select>
+    	<?php 
+	}
+
+	public function myplugin_settings_radio_field_callback(){
+		$get_radio_field = get_option( 'general_settings_radio_field' );
+    	?>
+    	<label for="value1">
+        	<input type="radio" name="general_settings_radio_field" value="value1" <?php checked( 'value1', $get_radio_field ); ?>/> Value 1
+    	</label>
+    	<label for="value2">
+        	<input type="radio" name="general_settings_radio_field" value="value2" <?php checked( 'value2', $get_radio_field ); ?>/> Value 2
+    	</label>
+    	<?php
 	}
 
 
